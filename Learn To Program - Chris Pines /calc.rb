4a92cae30606f41a1cc5dc8be8766b68b4029adc
Rami Aidy <ramiaidy@icloud.com>
2018-06-21 09:25:19 -0400

    numbers chapter of Chris Pines program

---
+++ Learn To Program - Chris Pines /calc.rb
@@ -0,0 +1,13 @@
+# Float arithmetic, results appear as a float
+puts 1.0 + 2.0
+puts 2.0 * 3.0
+puts 5.0 - 8.0
+puts 9.0 / 2.0
+
+
+# Spaces don't matter, easier to read
+puts 1+2
+puts 2*3
+puts 5-8
+puts 9/2
+# Always rounds down on division of integers
---
+++ Learn To Program - Chris Pines /numbers.rb
@@ -0,0 +1,14 @@
+#how many hours are in a year?
+puts 365 * 24
+
+#how many minutes are in a decade?
+puts 365 * 24 * 60 * 10
+
+#how many seconds old are you?
+puts (22 * 365 * 24 * 60 * 60) + (119 * 24 * 60 * 60) + (9 * 60 * 60) + (19 * 60)
+
+#how many chocolates do you hope to eat in your life? 
+puts 80 * 365 * 2
+
+# If I am 1298 million seconds old, how old am I?
+puts (1298000000/60/60/24/365)
