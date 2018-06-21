3cdf4ec372548333b597c529546a2a49180d8ada
Rami Aidy <ramiaidy@icloud.com>
2018-06-21 09:40:48 -0400

    letters chapter, Chris Pines program

---
+++ Learn To Program - Chris Pines /letters.rb
@@ -0,0 +1,32 @@
+# strings with nothing in them = empty strings
+puts 'Hello, world!'
+puts ''
+puts 'Good-bye.'
+
+# you can do arithmetic to strings
+puts 'I like' + 'apple pie'
+
+# you must put a space in order for it to show up in arithemetic
+puts 'I like' + ' apple pie.'
+# or
+puts 'I like ' + 'apple pie.'
+
+# you can multiply strings by numbers
+puts 'blink ' * 4
+
+# theres a difference between numbers and digits
+puts  12  +  12
+puts '12' + '12'
+puts '12  +  12'
+
+puts  2  *  5
+puts '2' *  5
+puts '2  *  5'
+
+# add a \ in cases like puts 'You're swell!'
+puts 'You\'re swell!'
+puts 'backslash at the end of a string:  \\'
+puts 'up\\down'
+puts 'up\down'
+puts 'up\'down'
+
